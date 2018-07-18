2c309ec13feb32f21185b2b3c5c24e6b9b45bf3c
Georgina Bianca Fiorentinos <geofio@live.co.za>
2018-07-18 16:09:35 +0200

    something has changed

---
+++ Prac 01/InputValidation/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InputValidation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("How often do you hear a drip drop (in seconds)?");
+                string n = Console.ReadLine();
+                int m;
+
+                if (Int32.TryParse(n, out m))
+                {
+
+
+                    double litres = 86400 / m * 0.00025;
+                    double glasses = litres / 0.250;
+                    double total = Math.Floor(glasses);
+
+                    string result = string.Format("This wastes {0} glasses of water per day.", total);
+
+                    Console.WriteLine(result);
+                    Console.ReadLine();
+                    break;
+                    
+                }
+
+                else
+                {
+                    Console.Write("Please try again.");
+                }
+
+            }
+
+
+
+        }
+
+
+
+
+
+    }
+}
+
+
