6bb3065fa365abd3d6875d8a545ab34554f44c6f
Georgina Bianca Fiorentinos <geofio@live.co.za>
2018-07-25 14:51:27 +0200

    changes have been made

--- Prac02/TaxesCalculator/Program.cs
+++ Prac02/TaxesCalculator/Program.cs
@@ -9,180 +9,159 @@ namespace TaxesCalculator
 	class Program
 	{
 		static void Main(string[] args)
-		{
+		{ 
+            // The next 3 lines of code gets input from the user as to what age he/she is and changes it to an int
 			Console.Write("How old are you?");
 			string age = Console.ReadLine();
 			int new_age = Convert.ToInt32(age);
 
+            // The next 3 lines of code gets input from the user as to what salary he/she earns and changes it to and int
 			Console.Write("What is your salary?");
 			string sal = Console.ReadLine();
 			int salary = Convert.ToInt32(sal);
-
-			int tax;
-
-			if ((salary >= 0) && (salary <= 4999)) ;
+            
+            // The next few conditional statements find out what salary bracket the inputed salary lies in, and then 
+            // uses age to determine what tax bracket he or she is in.
+			if ((salary >= 0) && (salary <= 4999)) 
 			{
-				if (new_age < 65) ;
+                Console.WriteLine("You have to pay R0 of tax.");
+            }
+
+			if ((salary >= 5000) && (salary <= 6499)) 
+			{
+				if (new_age < 65) 
 				{
-					tax = 0;
-				} 
-				else if ((new_age >= 65) && (new_age <= 75)) ;
+                    Console.WriteLine("You have to pay R13 of tax.");
+                } 
+				else
 				{
-					tax = 0;
-				}
-				else if (new_age > 75) ;
-				{
-					tax = 0;
-				}
+                    Console.WriteLine("You have to pay R0 of tax.");
+                }
+				
 			}
 
-			if ((salary >= 5000) && (salary <= 6499)) ;
+			if ((salary >= 6500) && (salary <= 7799)) 
 			{
-				if (new_age < 65) ;
+                if (new_age < 65)
+                {
+                    Console.WriteLine("You have to pay R250 of tax.");
+                }
+                else
+                {
+                    Console.WriteLine("You have to pay R0 of tax.");
+                }
+            }
+
+			if ((salary >= 7800) && (salary <= 9999)) 
+			{
+                if (new_age < 65)
+                {
+                    Console.WriteLine("You have to pay R620 of tax.");
+                }
+                else
+                {
+                    Console.WriteLine("You have to pay R0 of tax.");
+                }
+            }
+
+			if ((salary >= 10000) && (salary <= 13499)) 
+			{
+				if (new_age < 65) 
 				{
-					tax = 13;
-				} 
-				else if ((new_age >= 65) && (new_age <= 75)) ;
+                    Console.WriteLine("You have to pay R1200 of tax.");
+                } 
+				else if ((new_age >= 65) && (new_age <= 75)) 
 				{
-					tax = 0;
-				}
-				else if (new_age > 75) ;
+                    Console.WriteLine("You have to pay R600 of tax.");
+                }
+				else  
 				{
-					tax = 0;
-				}
+                    Console.WriteLine("You have to pay R400 of tax.");
+                }
 			}
 
-			if ((salary >= 6500) && (salary <= 7799)) ;
+			if ((salary >= 13500) && (salary <= 17899)) 
 			{
-				if (new_age < 65) ;
+				if (new_age < 65) 
 				{
-					tax = 250;
-				} 
-				else if ((new_age >= 65) && (new_age <= 75)) ;
+                    Console.WriteLine("You have to pay R2100 of tax.");
+                } 
+				else if ((new_age >= 65) && (new_age <= 75)) 
 				{
-					tax = 0;
-				}
-				else if (new_age > 75) ;
+                    Console.WriteLine("You have to pay R1500 of tax.");
+                }
+				else 
 				{
-					tax = 0;
-				}
+                    Console.WriteLine("You have to pay R1200 of tax.");
+                }
 			}
 
-			if ((salary >= 7800) && (salary <= 9999)) ;
+			if ((salary >= 17900) && (salary <= 19999)) 
 			{
-				if (new_age < 65) ;
-				{
-					tax = 620;
-				} 
-				else if ((new_age >= 65) && (new_age <= 75)) ;
-				{
-					tax = 0;
-				}
-				else if (new_age > 75) ;
-				{
-					tax = 0;
-				}
-			}
+                if (new_age < 65)
+                {
+                    Console.WriteLine("You have to pay R2700 of tax.");
+                }
+                else if ((new_age >= 65) && (new_age <= 75))
+                {
+                    Console.WriteLine("You have to pay R2000 of tax.");
+                }
+                else
+                {
+                    Console.WriteLine("You have to pay R1800 of tax.");
+                }
+            }
 
-			if ((salary >= 10000) && (salary <= 13499)) ;
+			if ((salary >= 20000) && (salary <= 39999)) 
 			{
-				if (new_age < 65) ;
-				{
-					tax = 1200;
-				} 
-				else if ((new_age >= 65) && (new_age <= 75)) ;
-				{
-					tax = 600;
-				}
-				else if (new_age > 75) ;
-				{
-					tax = 400;
-				}
-			}
+                if (new_age < 65)
+                {
+                    Console.WriteLine("You have to pay R8000 of tax.");
+                }
+                else if ((new_age >= 65) && (new_age <= 75))
+                {
+                    Console.WriteLine("You have to pay R7500 of tax.");
+                }
+                else
+                {
+                    Console.WriteLine("You have to pay R7300 of tax.");
+                }
+            }
 
-			if ((salary >= 13500) && (salary <= 17899)) ;
+            if ((salary >= 40000) && (salary <= 59999))
+            {
+                if (new_age < 65)
+                {
+                    Console.WriteLine("You have to pay R16000 of tax.");
+                }
+                else if ((new_age >= 65) && (new_age <= 75))
+                {
+                    Console.WriteLine("You have to pay R15600 of tax.");
+                }
+                else
+                {
+                    Console.WriteLine("You have to pay R15400 of tax.");
+                }
+            }
+
+            else if (salary >= 60000)
 			{
-				if (new_age < 65) ;
-				{
-					tax = 2100;
-				} 
-				else if ((new_age >= 65) && (new_age <= 75)) ;
-				{
-					tax = 1500;
-				}
-				else if (new_age > 75) ;
-				{
-					tax = 1200;
-				}
-			}
+                if (new_age < 65)
+                {
+                    Console.WriteLine("You have to pay R20000 of tax.");
+                }
+                else if ((new_age >= 65) && (new_age <= 75))
+                {
+                    Console.WriteLine("You have to pay R17050 of tax.");
+                }
+                else
+                {
+                    Console.WriteLine("You have to pay R17000 of tax.");
+                }
+            }
 
-			if ((salary >= 17900) && (salary <= 19999)) ;
-			{
-				if (new_age < 65) ;
-				{
-					tax = 2700;
-				} 
-				else if ((new_age >= 65) && (new_age <= 75)) ;
-				{
-					tax = 2000;
-				}
-				else if (new_age > 75) ;
-				{
-					tax = 1800;
-				}
-			}
-
-			if ((salary >= 20000) && (salary <= 39999)) ;
-			{
-				if (new_age < 65) ;
-				{
-					tax = 8000;
-				} 
-				else if ((new_age >= 65) && (new_age <= 75)) ;
-				{
-					tax = 7500;
-				}
-				else if (new_age > 75) ;
-				{
-					tax = 7300;
-				}
-			}
-
-			if ((salary >= 40000) && (salary <= 59999)) ;
-			{
-				if (new_age < 65) ;
-				{
-					tax = 16000;
-				} 
-				else if ((new_age >= 65) && (new_age <= 75)) ;
-				{
-					tax = 15600;
-				}
-				else if (new_age > 75) ;
-				{
-					tax = 15400;
-				}
-			}
-
-			if (salary >= 60000) ;
-			{
-				if (new_age < 65) ;
-				{
-					tax = 20000;
-				}
-				else if ((new_age >= 65) && (new_age <= 75)) ;
-				{
-					tax = 17050;
-				}
-				else if (new_age > 75) ;
-				{
-					tax = 17000;
-				}
-			}
-
-			string result = string.Format("You have to pay R(0) of TAX.", tax);
-			Console.WriteLine(result);
-			Console.ReadLine();
+			
+		Console.ReadLine();
 		}
 	}
 }
